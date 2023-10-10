59526cd634fc91dbc7230e11c8173b0e79b85400
JustSerhii <scimitarnew@gmail.com>
2023-10-10 22:05:21 +0300

    Size changed added

--- script.js
+++ script.js
@@ -1,19 +1,39 @@
 const cont = document.querySelector('.container');
+let size = 16;
 
-for (let i = 0; i < 16*16; i++){
-    const squares = document.createElement('div');
-    squares.className = 'square';
+const sizeButton = document.getElementById('size-button');
 
-    cont.append(squares);
+sizeButton.addEventListener('click', () => {
+    size = parseInt(prompt('Enter the size'));
+    if (size > 1 && size <= 100) {
+        clearGrid();
+        createGrid(size);
+    }
+    else alert('Size should be between 2 and 100, inclusive');
+})
+
+function clearGrid() {
+    const squares = document.querySelectorAll('.square');
+    squares.forEach(square => {
+        square.remove();
+    });
 }
 
-const squares1 = document.querySelectorAll('.square')
+function createGrid(size) {
+    cont.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    cont.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+    for (let i = 0; i < size * size; i++) {
+        const squares = document.createElement('div');
+        squares.className = 'square';
+        cont.append(squares);
+    }
 
-squares1.forEach(square => {
-    square.addEventListener('mouseover', () => {
-        square.className = 'changedSquare';
-    });
+}
+
+cont.addEventListener('mouseover', (event) => {
+    if (event.target.classList.contains('square')) {
+        event.target.classList.add('changedSquare');
+    }
 });
 
-
-
+createGrid(size);
