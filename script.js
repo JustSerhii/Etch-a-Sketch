385678f37fd4b56e5d9baef0cde14ac7d9ddac7f
Serhii <scimitarnew@gmail.com>
2023-10-14 20:26:11 +0300

    Background and drawing color pickers added

--- script.js
+++ script.js
@@ -1,38 +1,51 @@
-const cont = document.querySelector('.container');
+const cont = document.querySelector('.canva-container');
 let size = 16;
+let isDrawing = true; 
 
 const sizeButton = document.getElementById('size-button');
+const backColor = document.getElementById('background-color-picker');
+const drawColor = document.getElementById('drawing-color-picker');
 
 sizeButton.addEventListener('click', () => {
     size = parseInt(prompt('Enter the size'));
     if (size > 1 && size <= 100) {
         clearGrid();
         createGrid(size);
+    } else {
+        alert('Size should be between 2 and 100, inclusive');
     }
-    else alert('Size should be between 2 and 100, inclusive');
-})
+});
 
 function clearGrid() {
-    const squares = document.querySelectorAll('.square');
-    squares.forEach(square => {
-        square.remove();
-    });
+    cont.innerHTML = ''; 
 }
 
 function createGrid(size) {
     cont.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     cont.style.gridTemplateRows = `repeat(${size}, 1fr)`;
     for (let i = 0; i < size * size; i++) {
-        const squares = document.createElement('div');
-        squares.className = 'square';
-        cont.append(squares);
+        const square = document.createElement('div');
+        square.className = 'square';
+        square.style.backgroundColor = backColor.value; 
+        cont.appendChild(square);
     }
-
 }
 
+drawColor.addEventListener('input', () => {
+    isDrawing = true;
+});
+
+backColor.addEventListener('input', () => {
+    const squares = document.querySelectorAll('.square');
+    squares.forEach(square => {
+        square.style.backgroundColor = backColor.value;
+    });
+});
+
 cont.addEventListener('mouseover', (event) => {
-    if (event.target.classList.contains('square')) {
-        event.target.classList.add('changedSquare');
+    if (isDrawing && event.target.classList.contains('square')) {
+        event.target.style.backgroundColor = drawColor.value;
+        event.target.classList.add('changed-square');
     }
 });
 
